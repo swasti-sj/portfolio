cb193dccacd69db366b8611badb0a807eb6f728c
swasti-sj <jainswasti2004@gmail.com>
2025-10-17 00:34:14 +0530

    Refactor ResumeNew component by removing unused imports and state management for improved performance

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,50 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import SpinningGradient from "../gradient";
 
-// PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-// Replace with your Google Drive direct link
+// Replace with your Google Drive direct download link
 const pdfLink = "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID";
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <Container fluid className="resume-section">
       <Particle />
-      <Row style={{ justifyContent: "center", position: "relative", marginBottom: "20px" }}>
-         <h2 style={{ color: "white" }}>
-      👋 Have a look at my <span className="purple">Resume</span> below!
-    </h2>
-    <SpinningGradient lassName="sg-fixed-size" >
-        <Button variant="primary" href={pdfLink} target="_blank" style={{ maxWidth: "250px" }}>
-          <AiOutlineDownload /> &nbsp;Download CV
-        </Button>
+
+      <Row
+        style={{
+          justifyContent: "center",
+          position: "relative",
+          marginBottom: "20px",
+          textAlign: "center",
+        }}
+      >
+        <h2 style={{ color: "white" }}>
+          👋 Have a look at my <span className="purple">Resume</span> below!
+        </h2>
+
+        <SpinningGradient className="sg-fixed-size" >
+          <Button
+            variant="primary"
+            href={pdfLink}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload /> &nbsp;Download CV
+          </Button>
         </SpinningGradient>
       </Row>
-
-      {/* <Row className="resume" style={{ justifyContent: "center" }}>
-        <Document file={pdfLink}>
-          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-        </Document>
-      </Row>
-
-      <Row style={{ justifyContent: "center", position: "relative", marginTop: "20px" }}>
-        <Button variant="primary" href={pdfLink} target="_blank" style={{ maxWidth: "250px" }}>
-          <AiOutlineDownload /> &nbsp;Download CV
-        </Button>
-      </Row> */}
     </Container>
   );
 }
